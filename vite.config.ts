73967d8e69baa2e80642b58ea3063fae5f50e8f0
SenthilShelke <senthil.shelke@gmail.com>
2025-01-26 13:30:40 -0500

    updated vite.config.ts to work with netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative paths for all assets
+  base: '/',
   plugins: [react()],
+  server: {
+    port: 3000,    
+    open: true,         
+    cors: true,       
+  },
+  build: {
+    outDir: 'dist',      
+    sourcemap: true,   
+  },
 });
